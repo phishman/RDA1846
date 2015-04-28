ee464c92d35b96e312d1972281d9002cef3cff99
phishman <phishman@hotmail.com>
2015-04-28 14:56:26 -0400

    Initial Upload
    Initial Upload. Very much a work in progress. Any corrections,
    additions, changes would be very welcomed.

---
+++ RDA1846.cpp
@@ -0,0 +1,918 @@
+/*
+* Arduino Library for the RDA1846 VHF/UHF Transceiver Chip
+*
+* Copyright (C) 2015 RF Designs. All rights reserved.
+*
+* Author: Bob Frady <rfdesigns@live.com>
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public
+* License v2 as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public
+* License along with this program; if not, write to the
+* Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+* Boston, MA 021110-1307, USA.
+*
+* If you can't comply with GPLv2, alternative licensing terms may be
+* arranged. Please contact RF Designs <rfdesigns@live.com> for proprietary
+* alternative licensing inquiries.
+*/
+
+
+#include <wire.h>
+
+#include <RDA1846.h>
+
+
+void RDA1846::begin(void)
+{
+  _address = RDA_ADDRESS;
+  Wire.begin();
+}
+
+void RDA1846::writeWord(uint16_t data, uint8_t reg) {
+  uint8_t TempReg;
+  union {
+    uint8_t b[2];
+    uint16_t w;
+  } datau;
+  
+  TempReg = reg;
+  datau.w = data;
+
+  if(TempReg > 0x7F) {  		//RDA1846 uses only 7-bit register addresses. For addresses > 0x7F
+    uint16_t TempData=0x0001;  	//we must recursively call this function to enable register
+								//writes above 0x7F		
+	writeWord(TempData, 0x7F);
+	
+	reg &= 0x7F;
+  }
+  
+#ifdef DEBUGPRINTI2C
+    Serial.print("writeWord -- ");
+    Serial.print(_address);
+	Serial.print(":");
+	Serial.print(reg);
+	Serial.print(":");
+	if(data < 0x1000)
+	  Serial.print("0");
+	if(data < 0x0100)
+	  Serial.print("0");
+	if(data < 0x0010)
+	  Serial.print("0");
+	Serial.println(data, HEX);
+#endif
+
+  Wire.beginTransmission(_address);
+  I2CWRITE((uint8_t) reg);
+  
+  I2CWRITE((uint8_t) datau.b[1]);
+  I2CWRITE((uint8_t) datau.b[0]);
+  
+  Wire.endTransmission();
+  
+  if(TempReg > 0x7F) {  			//Reset back to register < 0x7F
+    uint16_t TempData=0x0000;  			   		
+	
+	writeWord(TempData, 0x7F);
+  }
+  
+  return;
+}
+
+bool RDA1846::readWord(uint16_t *data, uint8_t reg) {
+  uint8_t TempReg;
+  union {
+    uint8_t b[2];
+    uint16_t w;
+  } datau;
+  
+  TempReg = reg;
+  datau.w = *data;
+
+  if(TempReg > 0x7F) {  		//RDA1846 uses only 7-bit register addresses. For addresses > 0x7F
+    uint16_t TempData=0x0001;  	//we must call the writeWord function to enable register
+								//writes above 0x7F		
+	writeWord(TempData, 0x7F);
+	
+	reg &= 0x7F;
+  }
+
+  Wire.beginTransmission(_address);
+  I2CWRITE((uint8_t) reg);
+  Wire.endTransmission(0);
+  uint8_t timeout=0;
+  
+  Wire.requestFrom(_address, (uint8_t) 0x02);
+  while(Wire.available() < 2) {
+    timeout++;
+	if(timeout > I2CTIMEOUT) {
+	  return(true);
+	}
+	delay(1);
+  } 		//Experimental
+  
+  datau.b[1] = I2CREAD();
+  datau.b[0] = I2CREAD();
+  Wire.endTransmission(1);
+  
+  *data = datau.w;
+  
+  if(TempReg > 0x7F) {  			//Reset back to register < 0x7F
+    uint16_t TempData=0x0000;  			   		
+	
+	writeWord(TempData, 0x7F);
+  }
+  
+#ifdef DEBUGPRINTI2C
+    Serial.print("readWord -- ");
+    Serial.print(_address);
+	Serial.print(":");
+	Serial.print(reg);
+	Serial.print(":");
+	if(*data < 0x1000)
+	  Serial.print("0");
+	if(*data < 0x0100)
+	  Serial.print("0");
+	if(*data < 0x0010)
+	  Serial.print("0");
+	Serial.println(*data, HEX);
+#endif
+
+return(false);
+}
+
+
+void RDA1846::rdaRegDump(uint8_t start, uint8_t end) {
+  uint16_t val;
+  
+  Serial.println("Register Dump");
+  Serial.println("--- BEGIN DUMP ---");
+  for(uint8_t n=start;n <= end;n++) {
+    readWord(&val, n);
+	if(!(n%8)) {
+      Serial.print("\n");
+    }
+	if(n<16)
+	  Serial.print("0");
+    Serial.print(n, HEX);
+    Serial.print(":");
+	if(val < 0x1000)
+	  Serial.print("0");
+	if(val < 0x0100)
+	  Serial.print("0");
+	if(val < 0x0010)
+	  Serial.print("0");
+    Serial.print(val, HEX);
+    Serial.print(" ");
+  }
+  Serial.print("\n");
+  Serial.println("--- END DUMP ---");  
+}
+
+
+void RDA1846::rdaInit(void) {
+
+#ifdef DEBUGPRINT
+  Serial.println("--DEBUG: rdaInit Print---");
+#endif
+  for(int x=0; x<30; x++) {
+    writeWord(RDAinit[x].reg, RDAinit[x].data);
+#ifdef DEBUGPRINT
+    Serial.print(x);
+	Serial.print(":");
+	if(RDAinit[x].reg < 0x10)
+	  Serial.print("0");
+	Serial.print(RDAinit[x].reg, HEX);
+	Serial.print(":");
+	if(RDAinit[x].data < 0x1000)
+	  Serial.print("0");
+	if(RDAinit[x].data < 0x0100)
+	  Serial.print("0");
+	if(RDAinit[x].data < 0x0010)
+	  Serial.print("0");
+	Serial.println(RDAinit[x].data, HEX);
+#endif
+  }
+}
+
+
+void RDA1846::setRefClock(double freq) {		//Crystal Frequency in MHz
+  uint16_t xtal_freq, adclk_freq;
+  uint8_t mode;
+  
+  if((freq >= 12.0) && (freq <= 14.0)) {
+    xtal_freq = freq * 1000.0;
+	adclk_freq = freq * 500.0;  //(xtal_freq / 2) * 1000
+	mode = 1;
+  }
+  
+  if((freq >= 24.0) && (freq <= 28.0)) {
+    xtal_freq = freq * 500.0;  //(xtal_freq / 2) * 1000
+	adclk_freq = freq * 250.0;  //(xtal_freq / 4) * 1000
+	mode = 0;
+  }
+  
+  uint16_t tmp;
+  
+  writeWord(xtal_freq, REG_XTAL_FREQ);
+  writeWord(adclk_freq, REG_ADC_FREQ);
+  readWord(&tmp, REG_CLK_MODE);
+  if(mode)
+    bitSet(tmp, 0);
+  else
+    bitClear(tmp, 0);
+	
+  writeWord(tmp, REG_CLK_MODE);
+}
+
+
+void RDA1846::setFrequency(double freq) {  //Frequency in MHz Decimal okay. Automatically sets band.
+  union {
+    uint16_t w[2];
+    uint32_t l;
+  } datau;
+  
+  datau.l = freq * 8000;  // Freq(MHz) * 1000 * 8
+  
+  writeWord(datau.w[1], REG_FREQ_H);
+  writeWord(datau.w[0], REG_FREQ_L);
+  
+  uint16_t tmp;
+  readWord(&tmp, REG_RF_BAND);
+  if((freq >= 134.0) && (freq <= 174.0)) {
+    bitSet(tmp, 1);
+	bitSet(tmp, 0);
+  }
+  if((freq >= 200.0) && (freq <= 260.0)) {
+    bitSet(tmp, 1);
+	bitClear(tmp, 0);
+  }
+  if((freq >= 400.0) && (freq <= 520.0)) {
+    bitClear(tmp, 1);
+	bitClear(tmp, 0);
+  }
+  writeWord(tmp, REG_RF_BAND);
+}
+
+
+void RDA1846::setMode(uint16_t mode) {
+  writeWord(mode, REG_MODE);
+}
+
+uint16_t RDA1846::getMode(void) {
+  uint16_t mode;
+  
+  readWord(&mode, REG_MODE);
+  return(mode);
+}
+
+void RDA1846::setChannelMode(CHAN_MODE cmode) {
+  uint16_t mode;
+  
+  mode = getMode();
+  switch(cmode) {
+    case MODE25KHz:
+	  bitSet(mode, 12);
+	  bitSet(mode, 13);
+	  break;
+	case MODE125KHz:
+	  bitClear(mode, 12);
+	  bitClear(mode, 13);
+	  break;
+	default:
+	  break;
+  }
+  setMode(mode);
+}
+
+void RDA1846::setTxMode(bool cmode) {
+  uint16_t mode;
+  
+  mode = getMode();
+  if(cmode) {
+    bitSet(mode, 6);
+  } else {
+    bitClear(mode, 6);
+  }
+  setMode(mode);
+}
+
+void RDA1846::setRxMode(bool cmode) {
+  uint16_t mode;
+  
+  mode = getMode();
+  if(cmode) {
+    bitSet(mode, 5);
+  } else {
+    bitClear(mode, 5);
+  }
+  setMode(mode);
+}
+
+void RDA1846::setDeepSleep(bool cmode) {
+  uint16_t mode;
+  
+  mode = getMode();
+  if(cmode) {
+    bitSet(mode, 2);
+  } else {
+    bitClear(mode, 2);
+  }
+  setMode(mode);
+}
+
+void RDA1846::setTxChannel(TX_VOICE_CHAN cmode) {
+  uint16_t mode;
+  
+  readWord(&mode, REG_TXVOICE_CHAN);
+  switch(cmode) {
+    case MIC:
+	  bitClear(mode, 15);
+	  bitClear(mode, 14);
+	  break;
+	case INNERTONE2:
+	  bitClear(mode, 15);
+	  bitSet(mode, 14);
+	  break;
+	case GPIO1CODE:
+	  bitSet(mode, 15);
+	  bitClear(mode, 14);
+	  break;
+	default:
+	  break;
+  }
+  writeWord(mode, REG_TXVOICE_CHAN);
+}
+
+
+void RDA1846::setPABias(BIAS_VOLTAGE volts) {
+  uint16_t mode;
+  
+  readWord(&mode, REG_PABIAS);
+  
+  mode &= 0xFFFFFC00; 
+  mode |= volts;
+  
+  writeWord(mode, REG_PABIAS);
+}
+
+
+void RDA1846::setSubaudio(uint16_t mode) {
+
+  writeWord(mode, REG_SUBAUDIO);
+}
+
+
+void RDA1846::setSubaudioFreq(float freq) {
+  uint16_t ctcss_freq;
+  
+  ctcss_freq = freq * 2.0 * 16000;
+  
+  writeWord(ctcss_freq, REG_CTCSS_FREQ);
+}
+
+void RDA1846::setCDCSScode(uint32_t code) {
+  uint16_t upper, lower;
+  
+  upper = 0x00FF & (code >> 16);
+  lower = 0x0000FFFF & code;
+  
+  writeWord(upper, REG_CTCSS_CODE_H);
+  writeWord(lower, REG_CTCSS_CODE_L);
+}
+
+void RDA1846::setSquelch(bool sq) {
+  uint16_t mode;
+  
+  readWord(&mode, REG_MODE);
+  
+  if(sq) {
+    bitSet(mode, 3);
+  } else {
+    bitClear(mode, 3);
+  }
+  
+  writeWord(mode, REG_MODE);
+}
+
+void RDA1846::setCTCSSsel(bool sel) {
+  uint16_t val;
+  
+  readWord(&val, REG_SUBAUDIO);
+  if(sel) {
+    bitSet(val, 3);
+  } else {
+    bitClear(val, 3);
+  }
+   
+  writeWord(val, REG_SUBAUDIO);
+}
+
+void RDA1846::setCTCSSdet(bool det) {
+  uint16_t val;
+  
+  readWord(&val, REG_SUBAUDIO);
+  if(det) {
+    bitSet(val, 10);
+  } else {
+    bitClear(val, 10);
+  }
+  writeWord(val, REG_SUBAUDIO);
+}
+
+void RDA1846::setSquelchThreshold(uint16_t open, uint16_t shut) {
+
+  writeWord((open<<3), REG_SQ_H);
+  writeWord((shut<<3), REG_SQ_L);
+}
+
+void RDA1846::setSquelchThresholddBm(int16_t open, int16_t shut) {
+  uint16_t opendBm, shutdBm;
+  
+  opendBm = ((135 + open) & 0x0003FFFF);
+  shutdBm = ((135 + shut) & 0x0003FFFF);
+  
+  setSquelchThreshold(opendBm, shutdBm);
+}
+
+void RDA1846::setSquelchout(bool sq) {
+  uint16_t mode;
+  
+  readWord(&mode, REG_SQOUT_SEL);
+  
+  if(sq) {
+    bitSet(mode, 7);
+  } else {
+    bitClear(mode, 7);
+  }
+  
+  writeWord(mode, REG_SQOUT_SEL);
+}
+
+
+void RDA1846::setVOX(bool vox) {
+  uint16_t mode;
+  
+  readWord(&mode, REG_MODE);
+  
+  if(vox) {
+    bitSet(mode, 4);
+  } else {
+    bitClear(mode, 4);
+  }
+  
+  writeWord(mode, REG_MODE);
+}
+
+
+void RDA1846::setVOXThresholdmV(uint16_t open, uint16_t shut) {
+  uint16_t openVox, shutVox;
+  
+  openVox = ((225 * open) & 0xFFFF);
+  shutVox = ((225 * shut) & 0xFFFF);
+  
+  writeWord(openVox, REG_VOX_H);
+  writeWord(shutVox, REG_VOX_L);
+}
+
+void RDA1846::setTailNoiseElim(bool tn) {
+uint16_t mode;
+  
+  readWord(&mode, REG_MODE);
+  
+  if(tn) {
+    bitSet(mode, 11);
+  } else {
+    bitClear(mode, 11);
+  }
+  
+  writeWord(mode, REG_MODE);
+}
+
+void RDA1846::setTailNoisePhase(CTCSSPHASE phase) {
+  uint16_t val;
+  
+  readWord(&val, REG_SUBAUDIO);
+  switch(phase) {
+    case SHIFT120DEGREES:		// *** TO DO ***
+	case SHIFT180DEGREES:
+	case SHIFT240DEGREES:
+	default:
+	;
+  }
+  
+  writeWord(val, REG_SUBAUDIO);
+}
+
+void RDA1846::setDTMFMode(DTMF_MODE mode) {
+  uint16_t val;
+  
+  readWord(&val, REG_DTMF);
+  val &= 0xFCFF;
+  val |= mode << 8;
+  writeWord(val, REG_DTMF);
+}
+
+void RDA1846::setDTMFToneTime(uint8_t t) {
+  uint16_t val;
+  
+  t &= 0x0F;
+  readWord(&val, REG_DTMF);
+  val &= 0xFF0F;
+  val |= t << 4;
+  writeWord(val, REG_DTMF);
+}
+
+void RDA1846::setDTMFIdleTime(uint8_t t) {
+  uint16_t val;
+  
+  t &= 0x0F;
+  readWord(&val, REG_DTMF);
+  val &= 0xFFF0;
+  val |= t;
+  writeWord(val, REG_DTMF);
+}  
+
+void RDA1846::setDTMFTone1Freq(uint16_t freq) {
+
+  freq = freq * 4096;
+  
+  writeWord(freq, REG_TONE1_FREQ);
+}
+
+void RDA1846::setDTMFTone2Freq(uint16_t freq) {
+
+  freq = freq * 4096;
+  
+  writeWord(freq, REG_TONE2_FREQ);
+}
+
+void RDA1846::setDTMFIdle(bool i) {
+  uint16_t val;
+  
+  readWord(&val, REG_FLAG);
+  if(i) {
+    bitSet(val, 12);
+  } else {
+    bitClear(val, 12);
+  }
+  
+  writeWord(val, REG_FLAG);
+} 
+
+void RDA1846::setDTMFClk(DTMF_CLOCK clk) {
+
+  switch(clk) {
+    case CLK12POINT8MHZ:
+	case CLK25POINT6MHZ:
+	  writeWord((uint16_t)0x615D, REG_DTMF_C0_C1);
+	  writeWord(0x534D, REG_DTMF_C2_C3);
+	  writeWord(0x2C1E, REG_DTMF_C4_C5);
+	  writeWord(0x0AF6, REG_DTMF_C6_C7);
+	  break;
+	case CLK13MHZ:
+	case CLK26MHZ:
+	default:
+	  writeWord(0x615E, REG_DTMF_C0_C1);
+	  writeWord(0x574D, REG_DTMF_C2_C3);
+	  writeWord(0x311E, REG_DTMF_C4_C5);
+	  writeWord(0x0FFD, REG_DTMF_C6_C7);
+	  break;
+  }
+}
+
+void RDA1846::setTxFMDeviation(uint8_t dev) {
+  uint16_t currdev;
+  
+  readWord(&currdev, REG_TX_DEVIATION);
+  dev &= 0x7F;
+  currdev &= 0xE03F;
+  currdev |= dev << 6;
+  writeWord(currdev, REG_TX_DEVIATION);
+}
+
+
+void RDA1846::setTxCTCSSDeviation(uint8_t dev) {
+  uint16_t currdev;
+  
+  readWord(&currdev, REG_TX_DEVIATION);
+  dev &= 0x3F;
+  currdev &= 0xFFC0;
+  currdev |= dev;
+  writeWord(currdev, REG_TX_DEVIATION);
+}
+
+
+void RDA1846::setRxAnalogDACGain(uint8_t range) {
+  uint16_t currrange;
+  
+  readWord(&currrange, REG_RX_VOL);
+  range &= 0x0F;
+  currrange &= 0xFF0F;
+  currrange |= range << 4;
+  writeWord(currrange, REG_RX_VOL);
+}
+
+void RDA1846::setRxDigitalVoiceGain(uint8_t range) {
+  uint16_t currrange;
+  
+  readWord(&currrange, REG_RX_VOL);
+  range &= 0x0F;
+  currrange &= 0xFFF0;
+  currrange |= range;
+  writeWord(currrange, REG_RX_VOL);
+}
+
+void RDA1846::setGPIO7(GPIO7 mode) {
+  uint16_t val;
+  
+  readWord(&val, REG_GPIO);
+  switch(mode) {
+    case GP7_HIZ:
+	  bitClear(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case VOX:
+	  bitClear(val, 1);
+	  bitSet(val, 0);
+	  break;
+	case GP7_LOW:
+	  bitSet(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case GP7_HIGH:
+	  bitSet(val, 1);
+	  bitSet(val, 0);
+	  break;
+	default:
+	  break;
+  }
+  writeWord(val, REG_GPIO);
+}
+
+void RDA1846::setGPIO6(GPIO6 mode) {
+  uint16_t val;
+  
+  readWord(&val, REG_GPIO);
+  switch(mode) {
+    case GP6_HIZ:
+	  bitClear(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case SQ:
+	  bitClear(val, 1);
+	  bitSet(val, 0);
+	  break;
+	case GP6_LOW:
+	  bitSet(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case GP6_HIGH:
+	  bitSet(val, 1);
+	  bitSet(val, 0);
+	  break;
+	default:
+	  break;
+  }
+  writeWord(val, REG_GPIO);
+}
+
+void RDA1846::setGPIO5(GPIO5 mode) {
+  uint16_t val;
+  
+  readWord(&val, REG_GPIO);
+  switch(mode) {
+    case GP5_HIZ:
+	  bitClear(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case TXON_RF:
+	  bitClear(val, 1);
+	  bitSet(val, 0);
+	  break;
+	case GP5_LOW:
+	  bitSet(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case GP5_HIGH:
+	  bitSet(val, 1);
+	  bitSet(val, 0);
+	  break;
+	default:
+	  break;
+  }
+  writeWord(val, REG_GPIO);
+}
+
+void RDA1846::setGPIO4(GPIO4 mode) {
+  uint16_t val;
+  
+  readWord(&val, REG_GPIO);
+  switch(mode) {
+    case GP4_HIZ:
+	  bitClear(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case RXON_RF:
+	  bitClear(val, 1);
+	  bitSet(val, 0);
+	  break;
+	case GP4_LOW:
+	  bitSet(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case GP4_HIGH:
+	  bitSet(val, 1);
+	  bitSet(val, 0);
+	  break;
+	default:
+	  break;
+  }
+  writeWord(val, REG_GPIO);
+}
+
+void RDA1846::setGPIO3(GPIO3 mode) {
+  uint16_t val;
+  
+  readWord(&val, REG_GPIO);
+  switch(mode) {
+    case GP3_HIZ:
+	  bitClear(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case SDO:
+	  bitClear(val, 1);
+	  bitSet(val, 0);
+	  break;
+	case GP3_LOW:
+	  bitSet(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case GP3_HIGH:
+	  bitSet(val, 1);
+	  bitSet(val, 0);
+	  break;
+	default:
+	  break;
+  }
+  writeWord(val, REG_GPIO);
+}
+
+void RDA1846::setGPIO2(GPIO2 mode) {
+  uint16_t val;
+  
+  readWord(&val, REG_GPIO);
+  switch(mode) {
+    case GP2_HIZ:
+	  bitClear(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case GP2_INT:
+	  bitClear(val, 1);
+	  bitSet(val, 0);
+	  break;
+	case GP2_LOW:
+	  bitSet(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case GP2_HIGH:
+	  bitSet(val, 1);
+	  bitSet(val, 0);
+	  break;
+	default:
+	  break;
+  }
+  writeWord(val, REG_GPIO);
+}
+
+void RDA1846::setGPIO1(GPIO1 mode) {
+  uint16_t val;
+  
+  readWord(&val, REG_GPIO);
+  switch(mode) {
+    case GP1_HIZ:
+	  bitClear(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case CODE_IO:
+	  bitClear(val, 1);
+	  bitSet(val, 0);
+	  break;
+	case GP1_LOW:
+	  bitSet(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case GP1_HIGH:
+	  bitSet(val, 1);
+	  bitSet(val, 0);
+	  break;
+	default:
+	  break;
+  }
+  writeWord(val, REG_GPIO);
+}
+
+void RDA1846::setGPIO0(GPIO0 mode) {
+  uint16_t val;
+  
+  readWord(&val, REG_GPIO);
+  switch(mode) {
+    case GP0_HIZ:
+	  bitClear(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case CSS_IOC:
+	  bitClear(val, 1);
+	  bitSet(val, 0);
+	  break;
+	case GP0_LOW:
+	  bitSet(val, 1);
+	  bitClear(val, 0);
+	  break;
+	case GP0_HIGH:
+	  bitSet(val, 1);
+	  bitSet(val, 0);
+	  break;
+	default:
+	  break;
+  }
+  writeWord(val, REG_GPIO);
+}
+
+void RDA1846::setIntMode(uint16_t mode) {
+  writeWord(mode, REG_INT);
+}
+
+void RDA1846::setSTMode(STMODE mode) {
+  uint16_t val;
+  
+  readWord(&val, REG_MODE);
+  val &= 0xFCFF;
+  val |= mode << 8;
+  writeWord(val, REG_MODE);
+}
+
+void RDA1846::setEmphasis(bool emp) {
+  uint16_t val;
+  
+  readWord(&val, REG_VOICE_FILT);
+  if(emp) {
+    bitSet(val, 3);
+  } else {
+    bitClear(val, 3);
+  }
+  writeWord(val, REG_VOICE_FILT);
+}
+
+uint16_t RDA1846::getRSSI(void) {
+  uint16_t val;
+  
+  readWord(&val, REG_RSSI);
+  return (val & 0x03FF);
+}
+
+uint16_t RDA1846::getVSSI(void) {
+  uint16_t val;
+  
+  readWord(&val, REG_VSSI);
+  return (val & 0x07FF);
+}
+
+uint8_t RDA1846::getDTMFIndex(void) {
+  uint16_t val;
+  
+  readWord(&val, REG_DTMF_DET);
+  return ((val & 0x07E0)>>5);
+}
+
+uint8_t RDA1846::getDTMFCode(void) {
+  uint16_t val;
+  
+  readWord(&val, REG_DTMF_DET);
+  return (val & 0x000F);
+}
+
+uint16_t RDA1846::getFlags(void) {
+  uint16_t val;
+  
+  readWord(&val, REG_FLAG);
+  return (val);
+}
+
+uint16_t RDA1846::getChipID(void) {
+  uint16_t val;
+  
+  readWord(&val, REG_CHIP_ID);
+  return (val);
+}
+
+void RDA1846::reset(void) {
+  writeWord(0x0001, REG_MODE);
+}
---
+++ RDA1846.h
@@ -0,0 +1,370 @@
+#ifndef RDA1846_H
+#define RDA1846_H
+
+
+#define DEBUGPRINT
+//#define DEBUGPRINTI2C
+
+/* Retro-compatibility with arduino 0023 and previous version */
+#if ARDUINO >= 100
+#include "Arduino.h"
+#define I2CWRITE(x) Wire.write(x)
+#define I2CREAD() Wire.read()
+#else
+#include "WProgram.h"
+#define I2CWRITE(x) Wire.send(x)
+#define I2CREAD() Wire.receive()
+#endif
+
+
+#include <inttypes.h>
+
+#ifndef I2CTIMEOUT
+  #define I2CTIMEOUT 100
+#endif
+
+#ifndef RDA_SEN
+  #define RDA_SEN 0
+#endif
+
+#if RDA_SEN 
+  #define RDA_ADDRESS  0b0101110 //0x2E
+#else
+  #define RDA_ADDRESS  0x71 //0x71
+#endif
+
+//some default values may be in question
+//Registers attempted to be identified from various sources. Documentation leaves a lot to be desired...
+
+#define REG_CHIP_ID 0x00  	//Chip ID default:0x1846
+#define REG_MID 0x01 		//Metal Revision ID default:0x0007
+#define REG_ADC 0x03 		//default:0x2B51 
+							//BIT[12]tx_adc_reset_dr 1=enable direct reg 0=disable direct reg  
+							//BIT[11]tx_adc_reset_reg 1=enable reset adc 0=disable reset adc
+#define REG_CLK_MODE 0x04
+#define REG_LDO_BYPASS 0x08 	//default:0x02A0
+								//BIT[14] 1=bypass all LDOs inside RDA1846S. 0=normal. VHF Band must be 0.
+#define RGD_LDO_CONTROL 0x09 	//default:0x03C2
+								//BIT[9:7] ldo_dig_vbit If 08H[14]=0,control LDO Vout for digital. 100=2.20V 101=2.40V 110=2.80V 111=3.30V
+#define REG_PABIAS 0x0A  		//BIT[4:11]padrv_ibit 10:6pga_gain 5:0pabias_voltage
+#define REG_PLL_LOCK 0x0D 		//BIT[15:0]=Pll Unlock 1=pll lock (read only)
+#define REG_RF_BAND 0x0F
+#define REG_TUNING 0x15  //BIT[12:9]tuning_bit Tuning IF filter center freq & bw
+#define REG_GPIO 0x1F
+#define REG_PLL 0x24 //default:0x0001 
+					//BIT[15]pll_lock_det_sel BIT[14:13]reset_pll_lock_delay BIT[7]dsp_resetn_dr 6dsp_resetn_reg
+					//BIT[15] 1=bypass pll lock det function
+					//BIT[14:13] pll_lock_delay
+					// 00=10uS
+					// 01=20uS
+					// 10=30uS
+					// 11=40uS
+#define REG_FREQ_H 0x29
+#define REG_FREQ_L 0x2A  //default:3A84
+#define REG_XTAL_FREQ 0x2B
+#define REG_ADC_FREQ 0x2C
+#define REG_INT 0x2D
+#define REG_MODE 0x30  	//default:0x0000
+						//BIT[15] - other
+						//BIT[14] - xtal_mode 1=26MHz/13MHz 0=25.6MHz/12.8MHz
+						//BIT[13] - filter_band_sel Analog Filter Band 1=25khz 0=12.5khz
+						//BIT[12] - band_mode_sel DSP Band Mode Select 1=25khz 0=12.5khz
+						//BIT[11] - tail_elim_en 1=enable eliminate tail noise, 0= disable eliminate 
+						//BIT[10] - direct_reg 1=enable direct reg 0=disable
+						//BIT[9:8] - other
+						//BIT[7] - mute 1=mute 0=no mute
+						//BIT[6] - tx_on
+						//BIT[5] - rx_on
+						//BIT[4] - vox_on 1=enable vox 0=disable vox
+						//BIT[3] - sq_on 1=auto squelch 0=off
+						//BIT[2] - pdn_reg 1=follow PDN pin, 0=disbale
+						//BIT[1] - chip_cal_en 1=cal enable, 0=cal disable
+						//BIT[0] - soft_reset 1=reset-all registers reset to default, 0=normal
+#define REG_AGC 0x32 	//default:0x7497
+						//bits 11:6=agc_target_pwr unit=2dB
+#define REG_TONE1_FREQ 0x35
+#define REG_TONE2_FREQ 0x36
+#define REG_OUTSEL 0x3A //default:0x40C3
+						//BIT[15] code_out_sel 1=output code sample signal via GPIO3, 0=output dtmf_sample/dtmf_idle signal VIA GPIO3
+						//BIT[14:12] voice_sel voice path select
+						//BIT[11] sq_out_sel 1=gpio6=sq_cmp & sub_aud_cmp; 0=gpio6=sq_smp
+						//BIT[10:6] sq_dten sq condition enable
+						//BIT[5] ctcss/cdcss_out_sel - Select CTCSS/CDCSS mode for rx
+						//BIT[4:0] ctcss/cdcss_deten = Select CTCSS/CDCSS detect mode for Rx
+#define REG_TXVOICE_CHAN 0x3C
+#define REG_VOX_H 0x41 //default:0x4006  BIT[6:0]voice_gain_tx
+#define REG_VOX_L 0x42
+#define REG_TX_DEVIATION 0x43
+#define REG_RX_VOL 0x44 //default:00FF 
+						//BIT[11:8] Voice digital gain after tx ADC down sample
+						//BIT[7:4] Analog DAC gain
+						//BIT[3:0] Digital Voice Gain
+#define REG_SUBAUDIO 0x45
+#define REG_SQ_H 0x48
+#define REG_SQ_L 0x49
+#define REG_CTCSS_FREQ 0x4A
+#define REG_CTCSS_CODE_H 0x4B
+#define REG_CTCSS_CODE_L 0x4C 
+#define REG_CTCSS_PHASE 0x4E //default:20C2
+#define REG_SQOUT_SEL 0x54
+#define REG_VOICE_FILT 0x58
+#define REG_FLAG 0x5C
+#define REG_RSSI 0x5F
+#define REG_VSSI 0x60
+#define REG_DTMF 0x63
+#define REG_DTMF_C0_C1 0x66
+#define REG_DTMF_C2_C3 0x67
+#define REG_DTMF_C4_C5 0x68
+#define REG_DTMF_C6_C7 0x69
+#define REG_DTMF_DET 0x6C
+
+#define SUBAUDIODISABLE 0x0000
+#define INNERCTCSS 0x0001
+#define INNERCDCSS 0x0002
+#define OUTERCTCSS 0x0005
+#define OUTERCDCSS 0x0006
+#define SUBAUDIO_CMP_GPIO 0x0008
+#define SUBAUDIO_SDO_GPIO 0x0000
+#define CDCSS_24BIT 0x0010
+#define CDCSS_23BIT 0x0000
+#define CDCSS_INVERSE 0x0080
+#define CDCSS_POSITIVE 0x0800
+
+#define INT_CSS_CMP 0x0200
+#define INT_RXON_RF 0x0100
+#define INT_TXON_RF 0x0080
+#define INT_DTMF_IDLE 0x0040
+#define INT_CTCSS_PHASE 0x0020
+#define INT_IDLE_TIMEOUT 0x0010
+#define INT_RXON_RF_TIMEOUT 0x0008
+#define INT_SQ 0x0004
+#define INT_TXON_RF_TIMEOUT 0x0002
+#define INT_VOX 0x0001
+
+struct RDACommand {
+  uint8_t reg;
+  uint16_t data;
+};
+
+  enum CHAN_MODE {
+    MODE25KHz = 0x00,
+    MODE125KHz = 0x01
+  };
+  
+  enum TX_VOICE_CHAN {
+    MIC = 0x00,
+	INNERTONE2 = 0x01,
+	GPIO1CODE = 0x02
+  };
+  
+  enum BIAS_VOLTAGE {
+    BIAS101V = 0x00,
+	BIAS105V = 0x01,
+	BIAS109V = 0x02,
+	BIAS118V = 0x04,
+	BIAS134V = 0x08,
+	BIAS168V = 0x10,
+	BIAS245V = 0x20,
+	BIAS313V = 0x7F
+  };
+  
+  enum CTCSSPHASE {
+    SHIFT120DEGREES = 0x00,
+	SHIFT180DEGREES = 0x01,
+	SHIFT240DEGREES = 0x02
+  };
+
+  enum DTMF_MODE {
+    SINGLETONE = 0x03,
+	DUALTONE = 0x01,
+	DIABLE = 0x00
+  };
+
+  enum DTMF_CLOCK {
+    CLK12POINT8MHZ,
+	CLK25POINT6MHZ,
+	CLK13MHZ,
+	CLK26MHZ
+  };
+  
+  enum GPIO7 {
+    GP7_HIZ,
+	VOX,
+	GP7_LOW,
+	GP7_HIGH
+  };
+  
+  enum GPIO6 {
+    GP6_HIZ,
+    SQ,
+	GP6_LOW,
+	GP6_HIGH
+  };
+  
+  enum GPIO5 {
+    GP5_HIZ,
+	TXON_RF,
+	GP5_LOW,
+	GP5_HIGH
+  };
+  
+  enum GPIO4 {
+    GP4_HIZ,
+	RXON_RF,
+	GP4_LOW,
+	GP4_HIGH
+  };
+  
+  enum GPIO3 {
+    GP3_HIZ,
+	SDO,
+	GP3_LOW,
+	GP3_HIGH
+  };
+  
+  enum GPIO2 {
+    GP2_HIZ,
+	GP2_INT,
+	GP2_LOW,
+	GP2_HIGH
+  };
+  
+  enum GPIO1 {
+    GP1_HIZ,
+	CODE_IO,
+	GP1_LOW,
+	GP1_HIGH
+  };
+  
+  enum GPIO0 {
+    GP0_HIZ,
+	CSS_IOC,
+	GP0_LOW,
+	GP0_HIGH
+  };
+  
+  enum STMODE {
+    TXON_RXONAUTO = 0x10,
+	RXONAUTO_TXONMANUAL = 0x01,
+	TXONMANUAL_RXONMANUAL = 0x00
+  };
+
+class RDA1846 {
+public:
+
+struct RDACommand RDAinit[30] = {  //init values from http://elazary.com
+ { 0x30, 0x0001 }, 
+ { 0x30, 0x0004 },
+ { 0x04, 0x0FD0 },
+ { 0x0B, 0x1A10 },
+ { 0x2B, 0x32C8 },
+ { 0x2C, 0x1964 },
+ { 0x32, 0x627C },
+ { 0x33, 0x0AF2 },
+ { 0x47, 0x2C2F },
+ { 0x4E, 0x293A }, 
+ { 0x54, 0x1D4C },
+ { 0x56, 0x0652 }, 
+ { 0x6E, 0x062D },
+ { 0x70, 0x1029 },
+ { 0x7F, 0x0001 },
+ { 0x05, 0x001F },
+ { 0x7F, 0x0000 },
+ { 0x30, 0x3006 },
+ { 0x0A, 0x0400 },        //PA Bias 0000 0100 '00 00' 0000
+ { 0x1F, 0x0000 },        //GPIO selection 0001 1110 1011 1001
+ { 0x30, 0x3006 }, 
+ { 0x0F, 0x6be4 },
+ { 0x29, 0x0011 }, //145.525
+ { 0x2A, 0xC3A8 }, //145.525
+ { 0x48, 0x03FF }, //0000 0011 1111 0000
+ { 0x49, 0x01b3 }, //0003
+ { 0x3C, 0x0958 }, //0000 1001 0101 1000 
+ { 0x43, 0x1F1F },
+ { 0x30, 0x3006 },
+ { 0x36, 0x1000 },
+
+};
+
+uint16_t dtmfTone[10][2] = { //init values from http://elazary.com
+ { 5472, 3854 }, //0 
+ { 4952, 2855 }, //1 
+ { 5472, 2855 }, //2 
+ { 6050, 2855 }, //3 
+ { 4952, 3154 }, //4 
+ { 5472, 3154 }, //5 
+ { 6050, 3154 }, //6 
+ { 4952, 3490 }, //7 
+ { 5472, 3490 }, //8 
+ { 6050, 3490 }, //9 
+};
+
+
+  
+  void begin(void);
+  void writeWord(uint16_t data, uint8_t reg);
+  bool readWord(uint16_t *data, uint8_t reg);
+  void rdaRegDump(uint8_t start, uint8_t end);
+  void rdaInit(void);
+  void setRefClock(double freq);
+  void setFrequency(double freq);
+  void setMode(uint16_t mode);
+  uint16_t getMode(void);
+  void setChannelMode(CHAN_MODE cmode);
+  void setTxMode(bool cmode);
+  void setRxMode(bool cmode);
+  void setDeepSleep(bool cmode);
+  void setTxChannel(TX_VOICE_CHAN cmode);
+  void setPABias(BIAS_VOLTAGE volts);
+  void setSubaudio(uint16_t mode);
+  void setSubaudioFreq(float freq);
+  void setCDCSScode(uint32_t code);
+  void setSquelch(bool sq);
+  void setCTCSSsel(bool sel);
+  void setCTCSSdet(bool det);
+  void setSquelchThreshold(uint16_t open, uint16_t shut);
+  void setSquelchThresholddBm(int16_t open, int16_t shut);
+  void setSquelchout(bool sq);
+  void setVOX(bool vox);
+  void setVOXThresholdmV(uint16_t open, uint16_t shut);
+  void setTailNoiseElim(bool tn);
+  void setTailNoisePhase(CTCSSPHASE phase);
+  void setDTMFMode(DTMF_MODE mode);
+  void setDTMFToneTime(uint8_t t);
+  void setDTMFIdleTime(uint8_t t);
+  void setDTMFTone1Freq(uint16_t freq);
+  void setDTMFTone2Freq(uint16_t freq);
+  void setDTMFIdle(bool i);
+  void setDTMFClk(DTMF_CLOCK clk);
+  void setTxFMDeviation(uint8_t dev);
+  void setTxCTCSSDeviation(uint8_t dev);
+  void setRxAnalogDACGain(uint8_t range);
+  void setRxDigitalVoiceGain(uint8_t range);
+  void setGPIO7(GPIO7 mode);
+  void setGPIO6(GPIO6 mode);
+  void setGPIO5(GPIO5 mode);
+  void setGPIO4(GPIO4 mode);
+  void setGPIO3(GPIO3 mode);
+  void setGPIO2(GPIO2 mode);
+  void setGPIO1(GPIO1 mode);
+  void setGPIO0(GPIO0 mode);
+  void setIntMode(uint16_t mode);
+  void setSTMode(STMODE mode);
+  void setEmphasis(bool emp);
+  uint16_t getRSSI(void);
+  uint16_t getVSSI(void);
+  uint8_t getDTMFIndex(void);
+  uint8_t getDTMFCode(void);
+  uint16_t getFlags(void);
+  uint16_t getChipID(void);
+  void reset(void);
+  
+protected:
+
+private:
+  uint8_t _address;
+  
+};
+
+
+#endif
